b7d89eec3749cd0ee534e9d44bbed9842a52ffe3
Renato Girão <renato.sgirao@gmail.com>
2020-07-01 20:15:14 -0300

    first commit

---
+++ CleanSwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwiftTest
+//
+//  Created by Renato Savoia Girão on 01/07/20.
+//  Copyright © 2020 Renato Savoia Girão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
