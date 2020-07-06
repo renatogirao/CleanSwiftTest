b887174d808c34386d43dc217f355f2cdd7c342e
Renato Girão <renato.sgirao@gmail.com>
2020-07-06 11:33:03 -0300

    created filters and types of cards enum

---
+++ CleanSwiftTest/Interactor/FilterManager.swift
@@ -0,0 +1,9 @@
+//
+//  FilterManager.swift
+//  CleanSwiftTest
+//
+//  Created by Renato Savoia Girão on 05/07/20.
+//  Copyright © 2020 Renato Savoia Girão. All rights reserved.
+//
+
+import Foundation
